bf63beacd0346bcc03f604ee9dd008b7b974c9c7
Marie Burns <mcburns2222@gmail.com>
2019-02-17 20:46:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,6 @@
 class Application
   
-  @@items = []
+  @@item = []
   
   def call(env)
     resp = Rack::Response.new 
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items").last 
-      item = @@items.find{|i| i.name == item_name}
+      item = @@item.find{|i| i.name == item_name}
       
       resp.write item.price
     else
