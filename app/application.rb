af5f414d3224f63653ba2384a1f784c259018ac2
Marie Burns <mcburns2222@gmail.com>
2019-02-17 21:01:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,12 +12,7 @@ class Application
       resp.write item.price if item
       resp.write "Item not found" if !item
       resp.status = 400 if !item
-        # if !item 
-        #   resp.write "Item not found"
-        #   resp.status = 400
-        # else
-        #   resp.write item.price
-        # end
+        
     else
       resp.write "Route not found"
       resp.status = 404
