18ec033b0a7c7caf24d25e42adc494f34794ac03
Marie Burns <mcburns2222@gmail.com>
2019-02-17 20:58:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,8 +9,12 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       item = @@items.find{|i| i.name == item_name}
-      
-      resp.write item.price
+        if !item 
+          resp.write "No item found"
+          resp.status = 400
+        else
+          resp.write item.price
+        end
     else
       resp.write "Route not found"
       resp.status = 404
