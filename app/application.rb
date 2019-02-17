d7a5eb3e5586b6f8e1bbf2399b7a1dfcf0fb6b63
Marie Burns <mcburns2222@gmail.com>
2019-02-17 20:53:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
-      item = @@item.find{|i| i.name == item_name}
+      item = @@item.find{|s| s.name == item_name}
       
       resp.write item.price
     else
