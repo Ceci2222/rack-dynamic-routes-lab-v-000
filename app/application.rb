d3bb53de5dd295abd3d278fbbc7dceb1aa5c09f1
Marie Burns <mcburns2222@gmail.com>
2019-02-17 20:42:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,2 +1,10 @@
 class Application
   
+  @@items = []
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = 
